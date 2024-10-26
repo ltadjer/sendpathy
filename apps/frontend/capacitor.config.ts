c9f974d57ed17fe1119fbebc7ec8bf23e4a147e1
Liticia Tadjer <tadje@Mikasa>
2024-10-26 16:29:42 +0200

    feat(front): install capacitor & ionic

---
+++ apps/frontend/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    appId: 'fr.sendpathy.app',
+    appName: 'SendPathy',
+    webDir: 'dist',
+    bundledWebRuntime: false,
+};
+
+export default config;
